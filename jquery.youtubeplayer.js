7a2ef19555b8d8c626bd84d2a831d5c5b19b8354
NewPlayer <karasztamas@gmail.com>
2012-08-26 21:24:32 +0200

    init

---
+++ jquery.youtubeplayer.js
@@ -0,0 +1,117 @@
+(function( $ ) {
+
+	$.fn.youtubeplayer = function( options ) {
+	
+		// beállítások eszközölése
+		var settings = $.extend( {
+			'width': 400,
+			'height': 300,
+			'aspectratio': true
+		}, options );
+		
+		/**
+		 * A youtube video linkje
+		 */
+		var domain = '';
+		
+		/**
+		 * A youtube video id-je
+		 */
+		var youtubeid = '';
+		
+		/**
+		 * A YouTube api url-je
+		 */
+		var youtubeAPI = 'http://gdata.youtube.com/feeds/api/videos?v=2&alt=jsonc';
+		
+		/**
+		 * A youtube-tól visszakapott adatok
+		 */
+		var data = null;
+		
+		/**
+		 * A video aránya
+		 */
+		var ratio = 3/4;
+		
+		/**
+		 * Az eredeti elem, amire később tudunk hivatkozni
+		 */
+		var sender = null;
+		
+		// végigszaladunk az elemeken
+		return this.each(function() {
+			
+			sender = this;
+		
+			// Kihámozzuk az url-ből a video id-t
+			domain = $(this).attr( 'data-youtube' );
+			
+			youtubeid = domain.match(/(?:youtu\.be\/|youtube\.com(?:\/embed\/|\/v\/|\/watch\?v=|\/user\/\S+|\/ytscreeningroom\?v=|\/sandalsResorts#\w\/\w\/.*\/))([^\/&]{10,12})/)
+			
+			// ha kiderül, hogy ez nem egy szabványos youtube link, akkor megyünk a következőre
+			if( youtubeid == null ){
+				
+				return this;
+				
+			}else{
+
+				youtubeid = youtubeid[1];
+				
+			}
+			
+			$(this).addClass( 'youtubeplayer youtubeplayer-' + youtubeid );
+
+			// Lekérdezzük a videó adatokat a YouTube.com-tól
+			$.get( youtubeAPI,{ 'q':youtubeid },function(response){
+				
+				active_sender = sender;
+				
+				data = response.data;
+				
+				if( !data.totalItems ){
+					
+					// Ha a videó nem található
+					return this;
+					
+				}
+				
+				//TODO: mi történnyen ha nem engedélyezett a beágyazás?
+				// data.items[0].accessControl.embed != 'allowed'
+				
+				// mostmár megvan minden adat és tuti biztos, hogy használható is a kapott értékek
+				data = data.items[0];
+				
+				youtubeid = data.id;
+				
+				if( settings.aspectratio == true ){
+					
+					// video képarányainak ellenőrzése
+					ratio = 9/16;
+					if( data.aspectRatio != "widescreen" ){
+						
+						ratio = 3/4;
+						
+					}
+					
+					// magasság kiszámolása az adott videóhoz
+					settings.height = Math.round( settings.width * ratio );
+					
+				}
+				
+				$( '.youtubeplayer-' + youtubeid ).html(''+
+						'<div class="youtubeplayer-wrapper" style="background-image: url(\'' + data.thumbnail.hqDefault + '\')">' +
+							'<div class="youtube-play">' +
+							'</div>' +
+							'<div class="youtube-progressbar">' +
+							'</div>' +
+						'</div>');
+				
+			});
+			
+			
+		});
+		
+	};
+
+})( jQuery );
